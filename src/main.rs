79a35a38574f2788772416ae5d6e837b84a9160e
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-10-14 13:34:24 +0300

    Remove `env_logger` support for now

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@
 #![warn(missing_docs)]
 #![cfg_attr(feature = "cargo-clippy", warn(clippy))]
 
-extern crate env_logger;
-
 extern crate kvm;
 
 fn main() {
-    env_logger::init().expect("Failed to initialize logger");
 }
